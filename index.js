b57109df3fc7c976b014cad03d668a4a8222d950
Vishwajeet Kharote <vishwajeetkharote1196@gmail.com>
2019-02-05 03:07:08 -0800

    Added user registration and login

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 const db = require('./config/keys').mongoURI;
 const users = require('./routes/api/users');
 // connecting to mongoDB
 mongoose.connect(db)
         .then(()=>console.log('Successfully connected to db'))
         .catch(err=>console.log(err))
+
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
 
 // defining routes
 app.use('/api/users', users);
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,8 +1,83 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const gravatar = require('gravatar');
+const bycrypt = require('bcryptjs');
+const User = require('../../models/User');
+
+// @route GET/api/users/test
+// @desc testing route
+// @access public 
 
 router.get('/test', (req,res)=>{
+    console.log('this is working');
     res.send({msg:'This is test route'});
 });
+
+// @route POST/api/users/register
+// @desc register a new user
+// @access public
+router.post('/register', (req,res)=>{
+    User.findOne({email:req.body.email})
+        .then(user=>{
+            if(user){
+                return res.status(400).json({'msg':'user already exists'});
+            }
+            else{
+                const avatar = gravatar.url(req.body.email, {
+                    s:'200',
+                    r:'pg',
+                    d:'mm'
+                });
+
+                const newUser = new User({
+                    name:req.body.name,
+                    email:req.body.email,
+                    password:req.body.password,
+                    avatar
+                });
+                // newUser.save()
+                //     .then(user=>res.send(user))
+                //     .catch(err=>console.log(err))
+
+                bycrypt.genSalt(10, (err,salt)=>{
+                    bycrypt.hash(newUser.password, salt, (err,hash)=>{
+                        if(err)throw err;
+                        
+                        newUser.password = hash;
+                        newUser.save()
+                                .then(user=>res.send(user))
+                                .catch(err=>console.log(err))
+                        
+                    });
+                });
+            }
+        })
+});
+
+// @route GET/api/users/login
+// @desc Return JWT token after successful login 
+// @access public
+router.get('/login',(req,res)=>{
+    User.findOne({email:req.body.email})
+        .then(user=>{
+            if(!user){
+                res.status(400).send({'msg':'Account does not exist'});
+            }
+            bycrypt.compare(req.body.password, user.password)
+                .then(isMatch=>{
+                    if(isMatch){
+                        console.log(isMatch);
+                        res.json({msg:'User logged in'})
+                    }
+                    else{
+                        console.log(isMatch);
+                        return res.status(400).json({msg:'failed login'});
+                    }
+                })
+        })
+});
+
+
 
  module.exports = router;
